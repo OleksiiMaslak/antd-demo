c6b2ee578cbe980dfb1680fe8e0a192b2a7fe40b
Oleksii Maslak <maslak4it@gmail.com>
2023-05-03 15:41:12 +0300

    Сделал таблицу по шаблону, осталось доделать шапку

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,241 +1,267 @@
-import { Form, Radio, Space, Switch, Table } from 'antd';
-import { DownOutlined  } from '@ant-design/icons';
-import { useState } from 'react';
-const columns = [
+import React, { useState } from 'react';
+import { Table, Switch, Select } from 'antd';
+const { Option } = Select;
+
+const data = [
   {
-    title: 'Name',
-    dataIndex: 'name',
+    key: '1',
+    id: 1,
+    source: 'source 1',
+    broker: 'broker 1',
+    firstName: 'Alex',
+    lastName: 'Obama',
+    phone: '+380785699882',
+    email: "AlexObama@gmail.com",
+    firstBrokerStatus: 'Test Lead',
+    status: 'TEST',
+    color: '#fff',
+    backgroundColor: '#e91e63',
+    borderColor: '#e91e63',
+    brokerStatus: 'Test',
+    confirmedFTD: 'not',
+    country: 'CH',
+    date: '2023-04-25 12:18:28',
+    sendDate: '2023-04-26 15:27:48',
+    from: 'https://ant.design/components/table',
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    sorter: (a, b) => a.age - b.age,
+    key: '2',
+    id: 2,
+    source: 'source 2',
+    broker: 'broker 2',
+    firstName: 'Egor',
+    lastName: 'Biden',
+    phone: '+380785699882',
+    email: "EgorBiden@gmail.com",
+    firstBrokerStatus: 'Not Handled',
+    status: 'Broker Error',
+    color: '#fff',
+    backgroundColor: '#9e9e9e',
+    borderColor: '#9e9e9e',
+    brokerStatus: '',
+    confirmedFTD: 'not',
+    country: 'CH',
+    date: '2023-04-25 12:18:28',
+    sendDate: '2023-04-26 15:27:48',
+    from: 'https://ant.design/components/table',
+    
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    filters: [
-      {
-        text: 'London',
-        value: 'London',
-      },
-      {
-        text: 'New York',
-        value: 'New York',
-      },
-    ],
-    onFilter: (value, record) => record.address.indexOf(value) === 0,
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    sorter: true,
-    render: () => (
-      <Space size="middle">
-        <a>Delete</a>
-        <a>
-          <Space>
-            More actions
-            <DownOutlined />
-          </Space>
-        </a>
-      </Space>
-    ),
+    key: '3',
+    id: 3,
+    source: 'source 3',
+    broker: 'broker 3',
+    firstName: 'Andrew',
+    lastName: 'Trump',
+    phone: '+380785699882',
+    email: "AndrewTrump@gmail.com",
+    firstBrokerStatus: 'TEST',
+    status: 'New',
+    color: '#00bcd4',
+    backgroundColor: '#fff',
+    borderColor: '#00bcd4',
+    brokerStatus: 'Test',
+    confirmedFTD: 'not',
+    country: 'CH',
+    date: '2023-04-25 12:18:28',
+    sendDate: '2023-04-26 15:27:48',
+    from: 'https://ant.design/components/table',
   },
 ];
-const data = [];
-for (let i = 1; i <= 10; i++) {
-  data.push({
-    key: i,
-    name: 'John Brown',
-    age: Number(`${i}2`),
-    address: `New York No. ${i} Lake Park`,
-    description: `My name is John Brown, I am ${i}2 years old, living in New York No. ${i} Lake Park.`,
-  });
-}
-const defaultExpandable = {
-  expandedRowRender: (record) => <p>{record.description}</p>,
-};
-const defaultTitle = () => 'Here is title';
-const defaultFooter = () => 'Here is footer';
+
 const SiteForm = () => {
-  const [bordered, setBordered] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const [size, setSize] = useState('large');
-  const [expandable, setExpandable] = useState(defaultExpandable);
-  const [showTitle, setShowTitle] = useState(false);
-  const [showHeader, setShowHeader] = useState(true);
-  const [showfooter, setShowFooter] = useState(true);
-  const [rowSelection, setRowSelection] = useState({});
-  const [hasData, setHasData] = useState(true);
-  const [tableLayout, setTableLayout] = useState();
-  const [top, setTop] = useState('none');
-  const [bottom, setBottom] = useState('bottomRight');
-  const [ellipsis, setEllipsis] = useState(false);
-  const [yScroll, setYScroll] = useState(false);
-  const [xScroll, setXScroll] = useState();
-  const handleBorderChange = (enable) => {
-    setBordered(enable);
-  };
-  const handleLoadingChange = (enable) => {
-    setLoading(enable);
-  };
-  const handleSizeChange = (e) => {
-    setSize(e.target.value);
-  };
-  const handleTableLayoutChange = (e) => {
-    setTableLayout(e.target.value);
-  };
-  const handleExpandChange = (enable) => {
-    setExpandable(enable ? defaultExpandable : undefined);
-  };
-  const handleEllipsisChange = (enable) => {
-    setEllipsis(enable);
-  };
-  const handleTitleChange = (enable) => {
-    setShowTitle(enable);
-  };
-  const handleHeaderChange = (enable) => {
-    setShowHeader(enable);
-  };
-  const handleFooterChange = (enable) => {
-    setShowFooter(enable);
-  };
-  const handleRowSelectionChange = (enable) => {
-    setRowSelection(enable ? {} : undefined);
-  };
-  const handleYScrollChange = (enable) => {
-    setYScroll(enable);
-  };
-  const handleXScrollChange = (e) => {
-    setXScroll(e.target.value);
-  };
-  const handleDataChange = (newHasData) => {
-    setHasData(newHasData);
-  };
-  const scroll = {};
-  if (yScroll) {
-    scroll.y = 240;
+  const [checkedRows, setCheckedRows] = useState([]);
+  const [tableData, setTableData] = useState(data);
+
+  const handleSelectChange = (value, record) => {
+    const newData = [...tableData];
+    const index = newData.findIndex((item) => item.key === record.key);
+    if (index !== -1) {
+      newData[index] = {
+        ...newData[index],
+        firstBrokerStatus: value,
+      };
+      setTableData(newData);
+    }
   }
-  if (xScroll) {
-    scroll.x = '100vw';
-  }
-  const tableColumns = columns.map((item) => ({
-    ...item,
-    ellipsis,
-  }));
-  if (xScroll === 'fixed') {
-    tableColumns[0].fixed = true;
-    tableColumns[tableColumns.length - 1].fixed = 'right';
-  }
-  const tableProps = {
-    bordered,
-    loading,
-    size,
-    expandable,
-    title: showTitle ? defaultTitle : undefined,
-    showHeader,
-    footer: showfooter ? defaultFooter : undefined,
-    rowSelection,
-    scroll,
-    tableLayout,
+
+  const onSwitchChange = (checked, id) => {
+    const newCheckedRows = [...checkedRows];
+    const index = newCheckedRows.indexOf(id);
+    if (checked && index === -1) {
+      newCheckedRows.push(id);
+    } else if (!checked && index !== -1) {
+      newCheckedRows.splice(index, 1);
+    }
+    setCheckedRows(newCheckedRows);
   };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: '',
+      dataIndex: 'checkbox',
+      key: 'checkbox',
+      width: 25,
+      render: (text, record) => (
+        <Switch
+          checked={checkedRows.includes(record.id)}
+          onChange={(checked) => onSwitchChange(checked, record.id)}
+          checkedChildren={<span style={{ color: 'green' }}>Вкл</span>}
+          unCheckedChildren={<span style={{ color: 'red' }}>Выкл</span>}
+        />
+      ),
+    },
+    {
+      title: 'Source',
+      dataIndex: 'source',
+      key: 'source',
+      render: (_, record) => (
+        <Select
+          defaultValue={record.source}
+          style={{ width: 120 }}
+          onChange={(value) => {
+            const newData = [...data];
+            const index = newData.findIndex((item) => item.key === record.key);
+            if (index !== -1) {
+              newData[index] = {
+                ...newData[index],
+                source: value,
+              };
+              data.splice(index, 1, newData[index]);
+            }
+          }}
+        >
+          <Select.Option value="source 1">source 1</Select.Option>
+          <Select.Option value="source 2">source 2</Select.Option>
+          <Select.Option value="source 3">source 3</Select.Option>
+        </Select>
+      ),
+    },
+    {
+      title: 'Broker',
+      dataIndex: 'broker',
+      key: 'broker',
+      render: (_, record) => (
+        <Select
+          defaultValue={record.broker}
+          style={{ width: 120 }}
+          onChange={(value) => {
+            const newData = [...data];
+            const index = newData.findIndex((item) => item.key === record.key);
+            if (index !== -1) {
+              newData[index] = {
+                ...newData[index],
+                broker: value,
+              };
+              data.splice(index, 1, newData[index]);
+            }
+          }}
+        >
+          <Select.Option value="broker 1">broker 1</Select.Option>
+          <Select.Option value="broker 2">broker 2</Select.Option>
+          <Select.Option value="broker 3">broker 3</Select.Option>
+        </Select>
+      ),
+    },
+    {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      key: 'First Name',
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      key: 'Last Name',
+    },
+    {
+      title: 'phone',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'First Broker Status',
+      dataIndex: 'firstBrokerStatus',
+      key: 'firstBrokerStatus',
+      render: (_, record) => (
+        <Select
+          defaultValue={record.firstBrokerStatus}
+          style={{ width: 120 }}
+          onChange={(value) => handleSelectChange(value, record)}
+        >
+          <Option value="Test Lead">Test Lead</Option>
+          <Option value="Not Handled">Not Handled</Option>
+          <Option value="TEST">TEST</Option>
+        </Select>
+      ),
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      render: (text, record) => {
+        const style = {
+          color: record.color,
+          backgroundColor: record.backgroundColor,
+          border: record.borderColor ? `1px solid ${record.borderColor}` : '',
+          borderRadius: '4px',
+          padding: '8px',
+          textAlign: 'center',
+        };
+        return <div style={style}>{text}</div>;
+      },
+    },
+    {
+      title: 'broker Status',
+      dataIndex: 'brokerStatus',
+      key: 'broker Status',
+    },
+    {
+      title: 'Confirmed FTD',
+      dataIndex: 'confirmedFTD',
+      key: 'Confirmaed FTD',
+    },
+    {
+      title: 'Country',
+      dataIndex: 'country',
+      key: 'Country',
+    },
+    {
+      title: 'date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'send date',
+      dataIndex: 'sendDate',
+      key: 'send date',
+    },
+    {
+      title: 'from',
+      dataIndex: 'from',
+      key: 'from',
+    },
+  ];
+
   return (
     <>
-      <Form
-        layout="inline"
-        className="components-table-demo-control-bar"
-        style={{
-          marginBottom: 16,
-        }}
-      >
-        <Form.Item label="Bordered">
-          <Switch checked={bordered} onChange={handleBorderChange} />
-        </Form.Item>
-        <Form.Item label="loading">
-          <Switch checked={loading} onChange={handleLoadingChange} />
-        </Form.Item>
-        <Form.Item label="Title">
-          <Switch checked={showTitle} onChange={handleTitleChange} />
-        </Form.Item>
-        <Form.Item label="Column Header">
-          <Switch checked={showHeader} onChange={handleHeaderChange} />
-        </Form.Item>
-        <Form.Item label="Footer">
-          <Switch checked={showfooter} onChange={handleFooterChange} />
-        </Form.Item>
-        <Form.Item label="Expandable">
-          <Switch checked={!!expandable} onChange={handleExpandChange} />
-        </Form.Item>
-        <Form.Item label="Checkbox">
-          <Switch checked={!!rowSelection} onChange={handleRowSelectionChange} />
-        </Form.Item>
-        <Form.Item label="Fixed Header">
-          <Switch checked={!!yScroll} onChange={handleYScrollChange} />
-        </Form.Item>
-        <Form.Item label="Has Data">
-          <Switch checked={!!hasData} onChange={handleDataChange} />
-        </Form.Item>
-        <Form.Item label="Ellipsis">
-          <Switch checked={!!ellipsis} onChange={handleEllipsisChange} />
-        </Form.Item>
-        <Form.Item label="Size">
-          <Radio.Group value={size} onChange={handleSizeChange}>
-            <Radio.Button value="large">Large</Radio.Button>
-            <Radio.Button value="middle">Middle</Radio.Button>
-            <Radio.Button value="small">Small</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Table Scroll">
-          <Radio.Group value={xScroll} onChange={handleXScrollChange}>
-            <Radio.Button value={undefined}>Unset</Radio.Button>
-            <Radio.Button value="scroll">Scroll</Radio.Button>
-            <Radio.Button value="fixed">Fixed Columns</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Table Layout">
-          <Radio.Group value={tableLayout} onChange={handleTableLayoutChange}>
-            <Radio.Button value={undefined}>Unset</Radio.Button>
-            <Radio.Button value="fixed">Fixed</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Pagination Top">
-          <Radio.Group
-            value={top}
-            onChange={(e) => {
-              setTop(e.target.value);
-            }}
-          >
-            <Radio.Button value="topLeft">TopLeft</Radio.Button>
-            <Radio.Button value="topCenter">TopCenter</Radio.Button>
-            <Radio.Button value="topRight">TopRight</Radio.Button>
-            <Radio.Button value="none">None</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Pagination Bottom">
-          <Radio.Group
-            value={bottom}
-            onChange={(e) => {
-              setBottom(e.target.value);
-            }}
-          >
-            <Radio.Button value="bottomLeft">BottomLeft</Radio.Button>
-            <Radio.Button value="bottomCenter">BottomCenter</Radio.Button>
-            <Radio.Button value="bottomRight">BottomRight</Radio.Button>
-            <Radio.Button value="none">None</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-      </Form>
       <Table
-        {...tableProps}
-        pagination={{
-          position: [top, bottom],
-        }}
-        columns={tableColumns}
-        dataSource={hasData ? data : []}
-        scroll={scroll}
+        dataSource={data}
+        columns={columns}
+        scroll={{ x: 1570 }}
+        style={{ backgroundColor: '#fff', margin: '25px', padding: '25px' }}
       />
     </>
-  );
+);
 };
+
 export default SiteForm;
